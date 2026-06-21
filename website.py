d940f550535d10e832b7aa743dc4d515b4b039e4
Heemani <heemanipatil54@gmail.com>
2026-06-21 18:07:44 +0530

    fix render port

--- website.py
+++ website.py
@@ -195,4 +195,7 @@ def download():
 
     return send_file(pdf_file, as_attachment=True)
 
-app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
